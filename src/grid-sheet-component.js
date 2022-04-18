4dbba0e493483696e7a18b58d9edc2510cb9a62e
Eric Gade <eric.gade@gmail.com>
2022-04-18 14:36:34 -0400

    Updating grid based sheet with primitive editing example

--- src/grid-sheet-component.js
+++ src/grid-sheet-component.js
@@ -9,6 +9,17 @@ const templateString = `
 :host {
    display: grid;
    border: 1px red;
+   align-items: center;
+   justify-content: center;
+}
+
+::slotted(*){
+    color: green;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    text-overflow: ellipses;
 }
 </style>
 <slot></slot>
@@ -38,25 +49,31 @@ class GridSheet extends HTMLElement {
         this.primaryFrame = new PrimaryFrame(this.dataFrame, [0,0]);
         this.selector = new Selector(this.primaryFrame);
 
-        // Set up the resizing observer
-        this.observer = new ResizeObserver(this.onObservedResize);
-
         // Bind instace methods
         this.onObservedResize = this.onObservedResize.bind(this);
         this.render = this.render.bind(this);
+        this.dispatchSelectionChanged = this.dispatchSelectionChanged.bind(this);
+
+        // Bind event handlers
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
     connectedCallback(){
         if(this.isConnected){
             // Stuff up here
+            this.setAttribute('tabindex', '-1');
 
-            // init observer
+            // Set up the resizing observer
+            this.observer = new ResizeObserver(this.onObservedResize);
             this.observer.observe(this.parentElement);
         }
+
+        this.addEventListener('keydown', this.handleKeyDown);
     }
 
     disconnectedCallback(){
         this.observer.disconnect();
+        this.removeEventListener('keydown', this.handleKeyDown);
     }
 
     attributeChangedCallback(name, oldVal, newVal){
@@ -70,7 +87,16 @@ class GridSheet extends HTMLElement {
     }
 
     onObservedResize(info){
-        console.log(info);
+        // Attempt to re-set the number of columns and rows
+        // based upon the available free space in the element.
+        // Note that for now, we only attempt this on the
+        // horizontal (column) axis
+        console.log('resize');
+        let rect = this.getBoundingClientRect();
+        let currentCellWidth = this.cellWidth;
+        let newColumns = Math.floor(rect.width / currentCellWidth);
+        this.setAttribute('columns', newColumns);
+        this.render();
     }
 
     updateNumRows(){
@@ -95,6 +121,109 @@ class GridSheet extends HTMLElement {
         this.selector.primaryFrame = this.primaryFrame;
         this.selector.drawCursor();
         this.selector.updateElements();
+    }
+
+    dispatchSelectionChanged(){
+        let selectionEvent = new CustomEvent('selection-changed', {
+            detail: {
+                relativeCursor: this.selector.relativeCursor,
+                cursor: new Point(this.selector.cursor.x, this.selector.cursor.y),
+                frame: this.selector.selectionFrame,
+                data: this.selector.dataAtCursor
+            }
+        });
+        this.dispatchEvent(selectionEvent);
+    }
+
+    // Event Handling
+    // Event Handling
+    handleKeyDown(event){
+        console.log(event);
+        let isSelecting = event.shiftKey;
+
+        // Arrow Key to the Right
+        if(event.key == 'ArrowRight'){
+            if(event.ctrlKey){
+                this.selector.moveToRightEnd(isSelecting);
+            } else {
+                this.selector.moveRightBy(
+                    1,
+                    isSelecting
+                );
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
+
+        // Arrow key to the left
+        if(event.key == 'ArrowLeft'){
+            if(event.ctrlKey){
+                this.selector.moveToLeftEnd(isSelecting);
+            } else {
+                this.selector.moveLeftBy(
+                    1,
+                    isSelecting
+                );
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
+
+        // Arrow key downward
+        if(event.key == 'ArrowDown'){
+            if(event.ctrlKey){
+                this.selector.moveToBottomEnd(isSelecting);
+            } else {
+                this.selector.moveDownBy(
+                    1,
+                    isSelecting
+                );
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
+
+        // Arrow key up
+        if(event.key == 'ArrowUp'){
+            if(event.ctrlKey){
+                this.selector.moveToTopEnd(isSelecting);
+            } else {
+                this.selector.moveUpBy(
+                    1,
+                    isSelecting
+                );
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
+
+        // Page up
+        if(event.key == 'PageUp'){
+            if(event.altKey){
+                this.selector.pageLeft(isSelecting);
+            } else {
+                this.selector.pageUp(isSelecting);
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
+
+        // Page down
+        if(event.key == 'PageDown'){
+            if(event.altKey){
+                this.selector.pageRight(isSelecting);
+            } else {
+                this.selector.pageDown(isSelecting);
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            this.dispatchSelectionChanged();
+        }
     }
 
     static get observedAttributes(){
