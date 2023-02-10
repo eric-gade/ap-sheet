7adfe70ac1607f45c9705175da89003a44492023
darth-cheney <eric.gade@gmail.com>
2023-02-09 20:36:35 -0500

    Setting index for exports
    -- What
    We now export the actual APSheet component as the default, but we also
    want to export other associated classes for potential use elsewhere.

---
+++ index.js
@@ -0,0 +1,23 @@
+import Frame from "./src/Frame.js";
+import Point from "./src/Point.js";
+import Selector from "./src/Selector.js";
+import SelectionElement from "./src/SelectionElement.js";
+import Tab from "./src/Tab.js";
+import CursorElement from "./src/CursorElement.js";
+import SheetCell from "./src/SheetCell.js";
+import DataStore from "./src/DataStore.js";
+import APSheet from "./src/APSheet.js";
+
+export {
+    Frame,
+    Point,
+    Selector,
+    SelectionElement,
+    Tab,
+    CursorElement,
+    SheetCell,
+    DataStore,
+    APSheet,
+};
+
+export default APSheet;
